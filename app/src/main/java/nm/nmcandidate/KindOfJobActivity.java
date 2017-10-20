44e615a348e79a58e7504b1f913ed1ee612a4073
Pavan <dhavalchougale@gmail.com>
2017-10-20 19:13:29 +0530

    added profile details and kind of job pages

---
+++ app/src/main/java/nm/nmcandidate/KindOfJobActivity.java
@@ -0,0 +1,13 @@
+package nm.nmcandidate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KindOfJobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kind_of_job);
+    }
+}
---
+++ app/src/main/java/nm/nmcandidate/NMProfileDetailActivity.java
@@ -0,0 +1,13 @@
+package nm.nmcandidate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NMProfileDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nmprofile_detail);
+    }
+}
